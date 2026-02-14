8367f00a0eceb1d8edf805341ea159cd6ddd3d04
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-02-14 18:53:01 +0530

    Update page.jsx

--- login/page.jsx
+++ login/page.jsx
@@ -1,9 +1,9 @@
-"use client"
-import Login from "../components/login"
-export default function Loginpage(){
-    return(
-        <div>
-            <Login/>
-        </div>
-    )
+import { Suspense } from "react";
+import Login from "../components/login";
+export default function Loginpage() {
+  return (
+    <Suspense fallback={<div>Loading login...</div>}>
+      <Login />
+    </Suspense>
+  );
 }
