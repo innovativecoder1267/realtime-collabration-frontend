11f25bb9ce6ef5650766440bd065e3b33ec008d8
innovativecoder1267 <aaravchaprana1627@gmail.com>
2026-02-14 22:26:31 +0530

    Update page.jsx

--- src/app/forgot-password/page.jsx
+++ src/app/forgot-password/page.jsx
@@ -1,4 +1,7 @@
 import Forgot from "../components/forgot-password"
+import { Suspense } from "react"
 export default function ForgotPasswordPage() {
+  <Suspense fallback={<div>Loading forgot</div>}>
   return <Forgot/>
+  </Suspense>
 }
